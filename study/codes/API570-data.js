45bac6c8c3e5d07d8b3482f9daaad4bd03affee5
stacontrolee-prog <stacontrolee@gmail.com>
2025-09-21 19:36:25 -0400

    Add files via upload

---
+++ study/codes/API570-data.js
@@ -0,0 +1,5 @@
+// Registrar el código API 570
+registerCode('API570', {
+    title: "API 570 Piping Inspection Code, 5th Edition, February 2024",
+    titleEs: "Código de Inspección de Tuberías API 570, 5ta Edicion, Febrero 2024"
+});
---
+++ study/codes/API574-data.js
@@ -0,0 +1,5 @@
+// Registrar el código API 574
+registerCode('API574', {
+    title: "API 574 Inspection Practices for Piping System Components, 5th edition, February 2024",
+    titleEs: "Prácticas de Inspección para Componentes de Sistemas de Tuberías API 574, , 5ta edicion, Febrero 2024"
+});
