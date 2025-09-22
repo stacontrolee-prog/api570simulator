0b1e1a5171a28967164f89c261be0077e03d9ab6
stacontrolee-prog <stacontrolee@gmail.com>
2025-09-22 13:44:16 -0400

    Add files via upload

---
+++ study/js/codes/ASMEV-data.js
@@ -0,0 +1,5 @@
+// Registrar el código ASME V
+registerCode('ASMEV', {
+    title: "ASME Boiler and Pressure Vessel Code, Section V: Nondestructive Examination, 2023 Edition",
+    titleEs: "Código ASME para Calderas y Recipientes a Presión, Sección V: Exámenes No Destructivos, Edición 2023"
+});
