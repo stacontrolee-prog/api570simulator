098b40b44bebd80b388a08140f54023dfb5ccd7f
stacontrolee-prog <stacontrolee@gmail.com>
2025-10-09 21:46:20 -0400

    Add files via upload

---
+++ study/js/codes/ASMEPCC2-data.js
@@ -0,0 +1,5 @@
+// Registrar el código ASME PCC-2
+registerCode('ASMEPCC2', {
+    title: "ASME PCC-2: Repair of Pressure Equipment and Piping, 2022 Edition",
+    titleEs: "ASME PCC-2: Reparación de Equipos a Presión y Tuberías, Sección V: Exámenes No Destructivos, Edición 2022"
+});
